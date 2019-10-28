c22fbe37874594d46ab028059ffe02dc721add85
Sergey Yamshchikov <sergey.yamshchikov@scout24.com>
2019-10-28 19:05:54 +0100

    Initial implementation for AutoProxy Server

--- Sources/AutoProxy/main.swift
+++ Sources/AutoProxy/main.swift
@@ -3,12 +3,55 @@ import Kitura
 let router = Router()
 
 router.get("/") { request, response, next in
-    response.send(request.headers["user-agent"])
-    print(request.headers["user-agent"] as Any)
-    next()
+    guard let userAgent = request.headers["user-agent"] else {
+        response.statusCode = HTTPStatusCode.badRequest
+        next()
+        return
+    }
+    
+    let port = request.queryParameters["port"] ?? ""
+    let host = request.queryParameters["proxy"] ?? ""
+    let force = request.queryParameters["force"] ?? "false"
+    
+    if userAgent.lowercased().starts(with: "CFNetworkAgent".lowercased()) {
+        response.headers["Content-Type"] = "application/x-ns-proxy-autoconfig"
+        let body = """
+        function FindProxyForURL(url, host)
+        {
+            return "PROXY \(host):\(port); DIRECT";
+        }
+        
+        """
+        response.send(body)
+        next()
+    } else if userAgent.lowercased().starts(with: "Dalvik".lowercased()) {
+        response.headers["Content-Type"] = "application/x-ns-proxy-autoconfig"
+        let body = """
+        function FindProxyForURL(url, host)
+        {
+            return "PROXY \(host):\(port); DIRECT";
+        }
+        
+        """
+        response.send(body)
+        next()
+    } else if force == "true" {
+        response.headers["Content-Type"] = "application/x-ns-proxy-autoconfig"
+        let body = """
+        function FindProxyForURL(url, host)
+        {
+            return "PROXY \(host):\(port); DIRECT";
+        }
+        
+        """
+        response.send(body)
+        next()
+    }
+    else {
+        response.statusCode = HTTPStatusCode.badRequest
+        next()
+    }
 }
 
-Kitura.addHTTPServer(onPort: 8080, with: router)
+Kitura.addHTTPServer(onPort: 8081, with: router)
 Kitura.run()
-//Settings.shared.set(forKey: "hello", value: "Hey-hey, world!")
-//print(Settings.shared.string(forKey: "hello", or: "Wow"))
--- Sources/Model/Settings.swift
+++ Sources/Model/Settings.swift
@@ -2,11 +2,9 @@ import Foundation
 
 public class Settings {
 
-    //Optional("CFNetworkAgent (unknown version) CFNetwork/1107.1 Darwin/19.0.0")
-    //Optional("CFNetworkAgent (unknown version) CFNetwork/1111 Darwin/19.0.0 (x86_64)")
-    //Optional("CFNetworkAgent (unknown version) CFNetwork/1107.1 Darwin/19.0.0")
-    //Optional("CFNetworkAgent (unknown version) CFNetwork/1111 Darwin/19.0.0 (x86_64)")
-    //Optional("CFNetworkAgent (unknown version) CFNetwork/1111 Darwin/19.0.0 (x86_64)")
+    //"CFNetworkAgent (unknown version) CFNetwork/1107.1 Darwin/19.0.0"
+    //"CFNetworkAgent (unknown version) CFNetwork/1111 Darwin/19.0.0 (x86_64)"
+    //"Dalvik/2.1.0 (Linux; U; Android 9; Android SDK built for x86_64 Build/PSR1.180720.093)"
 
 
     public static let shared = Settings()
